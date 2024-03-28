f6b6f8fe6077b0818e8970df2ca5ff094dd850d2
moxiovo <634538850@qq.com>
2024-03-28 10:35:53 +0800

    Initial commit

--- day03-flow-control-app/src/com/fengtaisec/loop/WhileDemo1.java
+++ day03-flow-control-app/src/com/fengtaisec/loop/WhileDemo1.java
@@ -2,7 +2,11 @@ package com.fengtaisec.loop;
 
 public class WhileDemo1 {
     public static void main(String[] args) {
-        // 为什么有for 还用while循环
+        // 为什么有for 还用while循环书写格式，理解执行流程
+        int i = 0;
+        while (i < 5){
+            System.out.println("");
+        }
 
     }
 }
