c64de030a007ffcc968e486ead21263c373be621
moxiovo <634538850@qq.com>
2024-03-28 11:38:56 +0800

    Initial commit

---
+++ day03-flow-control-app/src/com/fengtaisec/loop/EndLessLoopDemo6.java
@@ -0,0 +1,17 @@
+package com.fengtaisec.loop;
+
+public class EndLessLoopDemo6 {
+    public static void main(String[] args) {
+        // 掌握死循环的写法
+        /*for( ; ; ){
+            System.out.println("hello");
+        }*/
+
+        /*while (true){
+            System.out.println("hello2");
+        }*/
+        do {
+            System.out.println("hello3");
+        }while (true);
+    }
+}
