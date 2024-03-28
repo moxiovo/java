91d0dadeef10ee248ef637a828118f305efd1f54
moxiovo <634538850@qq.com>
2024-03-28 10:42:10 +0800

    Initial commit

--- day03-flow-control-app/src/com/fengtaisec/loop/WhileDemo1.java
+++ day03-flow-control-app/src/com/fengtaisec/loop/WhileDemo1.java
@@ -3,9 +3,12 @@ package com.fengtaisec.loop;
 public class WhileDemo1 {
     public static void main(String[] args) {
         // 为什么有for 还用while循环书写格式，理解执行流程
+        // 迭代语句 is required for和while都能解决，反之亦然
+        // 规范：知道循环几次用for，不知道循环几次，建议使用：while
         int i = 0;
         while (i < 5){
-            System.out.println("");
+            System.out.println("Hello");
+            i++; // 死循环
         }
 
     }
