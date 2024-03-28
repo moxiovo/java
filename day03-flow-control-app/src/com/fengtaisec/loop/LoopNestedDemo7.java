63351972bc13b64b7acda8f39dafd723c4029141
moxiovo <634538850@qq.com>
2024-03-28 11:54:07 +0800

    Initial commit

--- day03-flow-control-app/src/com/fengtaisec/loop/LoopNestedDemo7.java
+++ day03-flow-control-app/src/com/fengtaisec/loop/LoopNestedDemo7.java
@@ -10,5 +10,17 @@ public class LoopNestedDemo7 {
             }
             System.out.println("--------------------");
         }
+    /** ****
+        ****
+        ****
+      **/
+        for (int i = 1; i <=4 ; i++) {
+            // i = 1 2 3
+            // 定义一个循环控制每行打印多少列星星
+            for (int j = 1; j <= 40 ; j++) {
+                System.out.print("*"); // 不换行
+            }
+            System.out.println();// 换行
+        }
     }
 }
