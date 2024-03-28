84c40b98ade5fb96a7b9a4dfa4eb7ae55aede373
moxiovo <634538850@qq.com>
2024-03-28 10:33:41 +0800

    Initial commit

--- day03-flow-control-app/src/com/fengtaisec/loop/ForDemo2.java
+++ day03-flow-control-app/src/com/fengtaisec/loop/ForDemo2.java
@@ -6,5 +6,30 @@ public class ForDemo2 {
         for (int i = 1; i <= 100; i++) {
             System.out.println(i);
         }
+        System.out.println("----------------------------");
+        // 求1-100的数据和
+        // 变量用于求和
+        int sum = 0; // 0
+        for (int i = 0; i <= 100; i++) {
+            sum += i; //累加给sum
+        }
+        System.out.println(sum);
+
+        // 定义一个循环产生1-100的奇数
+        int sum2 = 0; // 0
+        for (int i = 1; i <= 100; i+=2) {
+            sum2 += i; //累加给sum 1 3 5 7 9
+        }
+        System.out.println("1-100之间的奇数和:" + sum2);
+
+        System.out.println("--------------------------------");
+        // 方法2：
+        int sum3 = 0;
+        for (int i = 1; i <=100 ; i++) {
+            if (i % 2 == 0){
+                sum3 += i;
+            }
+        }
+        System.out.println("奇数和为：" + sum3);
     }
 }
---
+++ day03-flow-control-app/src/com/fengtaisec/loop/WhileDemo1.java
@@ -0,0 +1,8 @@
+package com.fengtaisec.loop;
+
+public class WhileDemo1 {
+    public static void main(String[] args) {
+        // 为什么有for 还用while循环
+
+    }
+}
