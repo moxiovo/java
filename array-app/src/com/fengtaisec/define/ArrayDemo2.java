3b034d79ad8d7c3f6cccc91f877967a95c428e0a
moxiovo <634538850@qq.com>
2024-03-28 16:39:15 +0800

    Initial commit

--- array-app/src/com/fengtaisec/define/ArrayDemo2.java
+++ array-app/src/com/fengtaisec/define/ArrayDemo2.java
@@ -2,7 +2,7 @@ package com.fengtaisec.define;
 
 public class ArrayDemo2 {
     public static void main(String[] args) {
-        // 目标：掌握数组的访问
+        // 目标：掌握数组的访问,数组遍历
         int[] arr = {12, 24, 36};
         for (int i = 0; i < arr.length; i++) {
             System.out.println(arr[i]);
