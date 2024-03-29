c2890467999d8351ac934991c78b5555fda71f34
moxiovo <634538850@qq.com>
2024-03-29 15:19:56 +0800

    Initial commit

---
+++ day05-method-app/src/com/fengtaisec/define/MethodDemo1.java
@@ -0,0 +1,9 @@
+package com.fengtaisec.define;
+
+public class MethodDemo1 {
+    public static void main(String[] args) {
+        // 掌握定义方法的完整格式，搞清楚使用方法的好处。
+        // 需求：很多程序员需要进行两个整数求和的操作。
+
+    }
+}
